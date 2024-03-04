b1e3b8aa949d14a1172e807f800c3ec36077cfd3
xypnox <xypnox@gmail.com>
2024-03-04 21:19:52 +0530

    Add basic style module css

---
+++ src/buttons/index.tsx
@@ -0,0 +1,33 @@
+import { JSX, splitProps } from "solid-js";
+
+import styles from "./styles.module.css";
+
+interface ButtonLocalProps {
+  icon?: boolean;
+  primary?: boolean;
+  size?: "small" | "medium" | "large";
+  rounded?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+}
+
+// Extend props of button component
+type ButtonProps = ButtonLocalProps & JSX.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = (props: ButtonProps) => {
+  const [local, ...rest] = splitProps(props, ["icon", "primary", "size", "rounded", "disabled", "loading"] as const)
+
+  return (
+    <button
+      class={styles.button}
+      classList={{
+        [styles.primary]: local.primary,
+        [styles.rounded]: local.rounded,
+        [styles.loading]: local.loading,
+        [styles.icon]: local.icon,
+      }}
+      disabled={local.disabled}
+      {...rest}
+    />
+  )
+};
